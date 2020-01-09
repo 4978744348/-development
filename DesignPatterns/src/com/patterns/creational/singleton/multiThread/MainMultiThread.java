274aeb38d7e78b9ca823e26616add83c95a4446d
Yauheni Baranou <javadev6891@gmail.com>
2020-01-09 21:09:15 +0300

    added description

--- DesignPatterns/src/com/patterns/creational/singleton/multiThread/MainMultiThread.java
+++ DesignPatterns/src/com/patterns/creational/singleton/multiThread/MainMultiThread.java
@@ -1,5 +1,10 @@
 package com.patterns.creational.singleton.multiThread;
 
+/**
+ * Singleton is a creational design pattern, 
+ * which ensures that only one object of its kind exists and 
+ * provides a single point of access to it for any other code
+ */
 public class MainMultiThread {
 
 	public static void main(String[] args) {
--- DesignPatterns/src/com/patterns/creational/singleton/singleThread/MainSingleThread.java
+++ DesignPatterns/src/com/patterns/creational/singleton/singleThread/MainSingleThread.java
@@ -1,5 +1,10 @@
 package com.patterns.creational.singleton.singleThread;
 
+/**
+ * Singleton is a creational design pattern, 
+ * which ensures that only one object of its kind exists and 
+ * provides a single point of access to it for any other code
+ */
 public class MainSingleThread {
 
 	public static void main(String[] args) {
